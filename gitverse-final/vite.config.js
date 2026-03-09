b87dada2ce19ecff98984a6debae8e76f70eecdb
Pramit2004 <155255603+Pramit2004@users.noreply.github.com>
2026-03-09 09:28:16 +0000

    upate vite.config.js - add host allowed list

--- gitverse-final/vite.config.js
+++ gitverse-final/vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+     strictPort: true,
+    allowedHosts: [
+      "gitverse-hwtv.onrender.com"
+    ],
+    hmr: {
+      clientPort: 443
+    }
   },
 })
+
+
